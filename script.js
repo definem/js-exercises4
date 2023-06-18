d97afe758cfee92a63a7863905c0104780af0e55
define) <120991965+definem@users.noreply.github.com>
2023-06-19 01:46:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -305,3 +305,13 @@ function rotateLeft3(nums){
 
   return nums;
 }
+
+// exercise commonEnd
+
+function commonEnd(a, b){
+  if(a[0] === b[0] || a[a.length - 1] === b[b.length - 1]){
+    return true
+  } else {
+    return false
+  }
+}
